da7b3d58363e4892223254d69045fcbc4ad0d9d0
Prénom NOM <prenom.nom@ens.uvsq.fr>
2021-11-18 10:47:49 +0100

    t

--- exercises/TD03_fonctions/TD3.py
+++ exercises/TD03_fonctions/TD3.py
@@ -2,11 +2,11 @@
 
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    return temps[1]*+temps[2]*+temps[3]*60+temps[4]
+    seconde = temps[0]*3600*24+temps[1]*3600+temps[2]*60+temps[3]
+    return seconde
 
-temps = (3,23,1,34)
-print(type(temps))
-print(tempsEnSeconde(temps))   
+mon_temps = (3,23,1,34)
+print(tempsEnSeconde(mon_temps))   
 
 def secondeEnTemps(seconde):
     """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
