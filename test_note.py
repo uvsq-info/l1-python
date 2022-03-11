3a0734a5fc12a4387750bbdd32a7ce09ddc749c4
Prénom NOM <prenom.nom@ens.uvsq.fr>
2022-03-11 10:38:04 +0100

    t

--- test_note.py
+++ test_note.py
@@ -1,7 +1,13 @@
-fic_in=open("/Users/bastien/Desktop/l1-python/notes_test.txt", "r")
-fic_out=open("moyenne","w")
+def moyenne(liste):
+    return (int(liste[1]) + int(liste[2]) + int(liste[3])) /3
+
+fic_in = open("notes_test.txt", "r")
+fic_out = open("moyenne","w")
+
 for ligne in fic_in:
-    print(ligne)
+    liste = ligne.split()
+    moy = moyenne(liste)
+    fic_out.write(ligne[:-1] + " " + str(moy) + "/n")
 
 fic_in.close()
 fic_out.close()
---
+++ word.py
@@ -0,0 +1,21 @@
+import random
+
+def nb_lignes(nom_fichier):
+    fic = open(nom_fichier, "r")
+    cpt = 0
+    for ligne in fic:
+        cpt +=1
+    fic.close()
+    return cpt
+
+def ecrit_liste_mots(n):
+    fic_in = open("word.txt" , "r")
+    fic_out = open("word" + str(n)+ ".txt", "w")
+    for ligne in fic_in:
+        if len(ligne) == n+1:
+            fic_out.write(ligne)
+    fic_in.close()
+    fic_out.close()
+
+ecrit_liste_mots(5)
+print(nb_lignes("words5.txt"))
