1e162b7f7fffd4efd3d6f6062f8b06a9baab757a
Bastien <bastiencombes92@gmail.com>
2023-04-11 12:04:40 +0200

    t

--- test.py
+++ test.py
@@ -1,207 +1,61 @@
+# Importation des modules nécessaires
 import tkinter as tk
 
-fenetre = tk()
-# Ajout d'un titre à la fenêtre principale :
-fenetre.title("Cryptanalyse")
-# Définir les dimensions par défaut la fenêtre principale :
-fenetre.geometry("640x480")
- # = fenetre pas redimensionnable dans longeur et largeur, figer les dimenssions
-fenetre.resizable(height=False, width=False)  
-# Ajout d'un texte dans la fenêtre :
-texte1 = Label (fenetre, text = "Veuillez choisir votre cryptage")
-texte1.pack()
+# Définition de la fonction de chiffrement César
+def chiffre_cesar(texte, cle):
 
-# Fonction bouton 1
+#Cette fonction prend en entrée un texte à chiffrer et une clé de chiffrement, et renvoie le texte chiffré selon la méthode de chiffrement César.
+    texte_chiffre = []
+    for lettre in texte:
+        if lettre.isalpha():
+# On calcule la position de la lettre dans l'alphabet, en prenant en compte
+# la casse (A = 0, a = 26, etc.)
+            if lettre.islower():
+                pos = ord(lettre) - 97
+            else:
+                pos = ord(lettre) - 65
+# On applique la clé de chiffrement (déplacement de 'cle' positions)
+            nouvelle_pos = (pos + cle) % 26
+# On convertit la nouvelle position en une lettre
+            if lettre.islower():
+                lettre_chiffree = chr(nouvelle_pos + 97)
+            else:
+                lettre_chiffree = chr(nouvelle_pos + 65)
+# On ajoute la lettre chiffrée au texte chiffré
+            texte_chiffre.append(lettre_chiffree)
+        else:
+# On ajoute les caractères spéciaux tels quels
+            texte_chiffre.append(lettre)
+    return "".join(texte_chiffre)
 
-def create_Cesar ():  
+# Définition de la fonction appelée lorsqu'on appuie sur le bouton Chiffrer
+def chiffrer():
+    texte_clair = zone_texte_clair.get("1.0", tk.END).strip()
+    cle = int(zone_cle.get())
+    texte_chiffre = chiffre_cesar(texte_clair, cle)
+    zone_texte_chiffre.delete("1.0", tk.END)
+    zone_texte_chiffre.insert("1.0", texte_chiffre)
 
-    """Création de la fonction permettant l'affichage du plateau de jeu lorsqu'on
-        clique sur le bouton du mode 1 joueur"""
+# Création de la fenêtre principale
+fenetre = tk.Tk()
+fenetre.title("Chiffrement César")
 
-    Cryptanalyse=Toplevel()
+# Création des widgets
+label_clair = tk.Label(fenetre, text="Texte clair:")
+zone_texte_clair = tk.Text(fenetre, width=40, height=5)
+label_cle = tk.Label(fenetre, text="Clé de chiffrement:")
+zone_cle = tk.Entry(fenetre)
+bouton_chiffrer = tk.Button(fenetre, text="Chiffrer", command=chiffrer)
+label_chiffre = tk.Label(fenetre, text="Texte chiffré:")
+zone_texte_chiffre = tk.Text(fenetre, width=40, height=5)
 
-    Cryptanalyse.geometry("640x480")#taille de la fenetre
+# Placement des widgets dans la fenêtre
+label_clair.grid(row=0, column=0, padx=10, pady=10)
+zone_texte_clair.grid(row=0, column=1, padx=10, pady=10)
+label_cle.grid(row=1, column=0, padx=10, pady=10)
+zone_cle.grid(row=1, column=1, padx=10, pady=10)
+bouton_chiffrer.grid(row=2, column=0, padx=10, pady=10)
+label_chiffre.grid(row=3, column=0, padx=10, pady=10)
+zone_texte_chiffre.grid(row=3, column=1, padx=10, pady=10)
 
-    Cryptanalyse.title("Cryptanalyse")#titre de la fenetre
-    texte2 = Label(Cryptanalyse,text ="Mettez votre message et entrez votre clé")
-    texte2.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    # entrée
-    value1 = StringVar() 
-    value1.set("texte par défaut")
-    entree1 = Entry(Cryptanalyse, textvariable= str, width=30)
-    entree1.pack()
-    bouton_crypter = Button (Cryptanalyse, text = "Crypter", command = cryptage)
-    bouton_crypter.pack()
-    # entrée
-    value2 = StringVar() 
-    value2.set("texte par défaut")
-    entree2 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree2.pack()
-    texte3 = Label(Cryptanalyse, text="Mettez votre message pour le dechiffreret et entrez votre clé")
-    texte3.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    value3 = StringVar() 
-    value3.set("texte par défaut")
-    entree3 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree3.pack()
-    bouton_decrypter = Button (Cryptanalyse, text= "Décrypter", command=decrypt_cesar)
-    bouton_decrypter.pack()
-    value4 = StringVar() 
-    value4.set("texte par défaut")
-    entree4 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree4.pack()
-
-def create_Vigenere ():  
-
-    """Création de la fonction permettant l'affichage du plateau de jeu lorsqu'on
-        clique sur le bouton du mode 1 joueur"""
-
-    Cryptanalyse=Toplevel()
-
-    Cryptanalyse.geometry("640x480")#taille de la fenetre
-
-    Cryptanalyse.title("Cryptanalyse")#titre de la fenetre
-    texte2 = Label(Cryptanalyse,text ="Mettez votre message et entrez votre clé")
-    texte2.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    # entrée
-    value1 = StringVar() 
-    value1.set("texte par défaut")
-    entree1 = Entry(Cryptanalyse, textvariable= str, width=30)
-    entree1.pack()
-    bouton_crypter = Button (Cryptanalyse, text = "Crypter")
-    bouton_crypter.pack()
-    # entrée
-    value2 = StringVar() 
-    value2.set("texte par défaut")
-    entree2 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree2.pack()
-    texte3 = Label(Cryptanalyse, text="Mettez votre message pour le dechiffreret et entrez votre clé")
-    texte3.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    value3 = StringVar() 
-    value3.set("texte par défaut")
-    entree3 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree3.pack()
-    bouton_decrypter = Button (Cryptanalyse, text= "Décrypter")
-    bouton_decrypter.pack()
-    value4 = StringVar() 
-    value4.set("texte par défaut")
-    entree4 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree4.pack()
-
-def create_Scytal ():  
-
-    """Création de la fonction permettant l'affichage du plateau de jeu lorsqu'on
-        clique sur le bouton du mode 1 joueur"""
-
-    Cryptanalyse=Toplevel()
-
-    Cryptanalyse.geometry("640x480")#taille de la fenetre
-
-    Cryptanalyse.title("Cryptanalyse")#titre de la fenetre
-    texte2 = Label(Cryptanalyse,text ="Mettez votre message et entrez votre clé")
-    texte2.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    # entrée
-    value1 = StringVar() 
-    value1.set("texte par défaut")
-    entree1 = Entry(Cryptanalyse, textvariable= str, width=30)
-    entree1.pack()
-    bouton_crypter = Button (Cryptanalyse, text = "Crypter")
-    bouton_crypter.pack()
-    # entrée
-    value2 = StringVar() 
-    value2.set("texte par défaut")
-    entree2 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree2.pack()
-    texte3 = Label(Cryptanalyse, text="Mettez votre message pour le dechiffreret et entrez votre clé")
-    texte3.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    value3 = StringVar() 
-    value3.set("texte par défaut")
-    entree3 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree3.pack()
-    bouton_decrypter = Button (Cryptanalyse, text= "Décrypter")
-    bouton_decrypter.pack()
-    value4 = StringVar() 
-    value4.set("texte par défaut")
-    entree4 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree4.pack()
-
-def create_Submonoalpha ():  
-
-    """Création de la fonction permettant l'affichage du plateau de jeu lorsqu'on
-        clique sur le bouton du mode 1 joueur"""
-
-    Cryptanalyse=Toplevel()
-
-    Cryptanalyse.geometry("640x480")#taille de la fenetre
-
-    Cryptanalyse.title("Cryptanalyse")#titre de la fenetre
-    texte2 = Label(Cryptanalyse,text ="Mettez votre message et entrez votre clé")
-    texte2.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    # entrée
-    value1 = StringVar() 
-    value1.set("texte par défaut")
-    entree1 = Entry(Cryptanalyse, textvariable= str, width=30)
-    entree1.pack()
-    bouton_crypter = Button (Cryptanalyse, text = "Crypter")
-    bouton_crypter.pack()
-    # entrée
-    value2 = StringVar() 
-    value2.set("texte par défaut")
-    entree2 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree2.pack()
-    texte3 = Label(Cryptanalyse, text="Mettez votre message pour le dechiffreret et entrez votre clé")
-    texte3.pack()
-    cle1 = Spinbox(Cryptanalyse, from_=0, to=26)
-    cle1.pack()
-    value3 = StringVar() 
-    value3.set("texte par défaut")
-    entree3 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree3.pack()
-    bouton_decrypter = Button (Cryptanalyse, text= "Décrypter")
-    bouton_decrypter.pack()
-    value4 = StringVar() 
-    value4.set("texte par défaut")
-    entree4 = Entry(Cryptanalyse, textvariable=str, width=30)
-    entree4.pack()
-# Ajout d'un bouton dans la fenêtre :
-bouton_Cesar = Button (fenetre, text = "Code Cesar", command = create)
-bouton_Cesar = Button (fenetre, text = "Code Cesar", command = create_Cesar)
-bouton_Cesar.pack()
-bouton_Vigenere = Button (fenetre, text = "Chiffre Vigenere", command= create)
-bouton_Vigenere = Button (fenetre, text = "Chiffre Vigenere", command= create_Vigenere)
-bouton_Vigenere.pack()
-bouton_Scytal = Button (fenetre, text = "Scytal", command = create)
-bouton_Scytal = Button (fenetre, text = "Scytal", command = create_Scytal)
-bouton_Scytal.pack()
-bouton_Submonoalpha = Button (fenetre, text = "Substituion Monoalphabetique", command = create)
-bouton_Submonoalpha = Button (fenetre, text = "Substituion Monoalphabetique", command = create_Submonoalpha)
-bouton_Submonoalpha.pack()
-
-def create():
-    newfenetre = tk.Toplevel()
-    labelExample = tk.Label(newWindow, text = "New Window")
-    buttonExample = tk.Button(newWindow, text = "New Window button")
-#def create():
-    #newfenetre = Toplevel(fenetre)
-    #newfenetre.pack()
-    #labelExample = Label(fenetre, text = "New Window")
-    #labelExample.pack()
-    #buttonExample = Button(fenetre, text = "New Window button")
-    #buttonExample.pack()
-    #labelExample.grid(column= 0, row = 0)
-
-# Affichage de la fenêtre créée : 
 fenetre.mainloop()
