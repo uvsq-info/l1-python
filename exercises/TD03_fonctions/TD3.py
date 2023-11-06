a3ac7de16c01f597e40909dc981835508cf07662
Sabri KABANI <sabri.kabani@ens.uvsq.fr>
2023-11-06 10:17:50 +0100

    correction 061123

--- exercises/TD03_fonctions/TD3.py
+++ exercises/TD03_fonctions/TD3.py
@@ -9,10 +9,20 @@ temps = (3,23,1,34)
 print(type(temps))
 print(tempsEnSeconde(temps))   
 
+#def secondeEnTemps(seconde):
+#    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
+#    return (seconde//86400, seconde//3600, seconde//60, seconde//1)
+#    pass
+
 def secondeEnTemps(seconde):
     """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
-    return (seconde//86400, seconde//3600, seconde//60, seconde//1)
-    pass
-    
+    jour=seconde//(24*60*60)
+    reste=seconde%(24*60*60)
+    heure=reste//(60*60)
+    reste1=reste%(60*60)
+    minute=reste1//60
+    seconde=reste1%60
+    return(jour,heure,minute,seconde)
+
 temps = secondeEnTemps(100000)
 print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
